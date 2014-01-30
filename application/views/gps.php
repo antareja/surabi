a6b2fac780ab10c5800c913725f70eb5b6a06c79
haidar mar'ie <coder5@ymail.com>
2014-01-30 16:48:49 +0700

    OPEN - task fixed #4: Template header footer bootstrap 3 http://github.com/antareja/surabi/issues/issue/4

--- application/views/gps.php
+++ application/views/gps.php
@@ -1,5 +1,14 @@
-<?php
+
+<!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/index.html#navbar">View navbar docs &raquo;</a>
+        </p>
+        <?php
 		foreach ($user as $users) {
-		 echo $users->username;
+		 echo '<p>'.$users->username.'</p>';
 		 }
 		 ?>
+      </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,1 +1,12 @@
-<?php echo 'test footer'; ?>
+</div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo site_url()?>assets/js/jquery.js"></script>
+    <script src="<?php echo site_url()?>assets/js/bootstrap.min.js"></script>
+  </body>
+
+<!-- Mirrored from getbootstrap.com/examples/navbar/ by HTTrack Website Copier/3.x [XR&CO'2013], Wed, 02 Oct 2013 12:14:32 GMT -->
+</html>
