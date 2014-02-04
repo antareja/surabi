404b4ca2b0a7b5cd98723e7eef034bf250949f8b
rizki182 <rizkifaishal82@gmail.com>
2014-02-04 16:56:05 +0700

    fixed #8 create icon

--- application/controllers/admin/sys_config.php
+++ application/controllers/admin/sys_config.php
@@ -1,16 +1,20 @@
 <?php
 if (! defined('BASEPATH'))
 	exit('No direct script access allowed');
+
 class Sys_config extends CI_Controller {
+
 	function __construct() {
 		parent::__construct();
 		$this->load->model('madmin');
 		$this->madmin = new Madmin();
 	}
+
 	public function index() {
 		$this->company_data();
 	}
-	public function company_data($users=NULL) {
+
+	public function company_data($users = NULL) {
 		$data['pageTitle'] = 'General Company Data';
 		$post = $this->input->post();
 		if ($post) {
@@ -18,13 +22,13 @@ class Sys_config extends CI_Controller {
 			$this->madmin->insertCompanyData($post);
 		} elseif ($users) {
 			$data['users'] = $this->madmin->getUser();
-			$this->load->template("admin/sys_config/general",$data);
+			$this->load->template("admin/sys_config/general", $data);
 		} else {
-			$this->load->template("admin/sys_config/general",$data);
+			$this->load->template("admin/sys_config/general", $data);
 		}
 	}
-//-------------------------------------icon---------------------------
-	public function icon($users=NULL) {
+	// -------------------------------------icon---------------------------
+	public function icon($users = NULL) {
 		$data['pageTitle'] = 'Icon';
 		$post = $this->input->post();
 		if ($post) {
@@ -32,13 +36,13 @@ class Sys_config extends CI_Controller {
 			$this->madmin->insertCompanyData($post);
 		} elseif ($users) {
 			$data['users'] = $this->madmin->getUser();
-			$this->load->template("admin/sys_config/icon",$data);
+			$this->load->template("admin/sys_config/icon", $data);
 		} else {
-			$this->load->template("admin/sys_config/icon",$data);
+			$this->load->template("admin/sys_config/icon", $data);
 		}
 	}
-//-----------------------------------Hardware--------------------------
-	public function hardware_type($users=NULL) {
+	// -----------------------------------Hardware--------------------------
+	public function hardware_type($users = NULL) {
 		$data['pageTitle'] = 'Hardware Type';
 		$post = $this->input->post();
 		if ($post) {
@@ -46,9 +50,9 @@ class Sys_config extends CI_Controller {
 			$this->madmin->insertCompanyData($post);
 		} elseif ($users) {
 			$data['users'] = $this->madmin->getUser();
-			$this->load->template("admin/sys_config/hardware_type",$data);
+			$this->load->template("admin/sys_config/hardware_type", $data);
 		} else {
-			$this->load->template("admin/sys_config/hardware_type",$data);
+			$this->load->template("admin/sys_config/hardware_type", $data);
 		}
 	}
 }
