b08d168507d1b3991978b9f74c4492f2582b6e46
haidar mar'ie <coder5@ymail.com>
2014-02-04 14:46:04 +0700

    OPEN - task 6: Recieve Packets Data From Phyton http://github.com/antareja/surabi/issues/issue/6

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,16 +1,20 @@
 <?php
 if (! defined('BASEPATH'))
 	exit('No direct script access allowed');
+
 class Admin extends CI_Controller {
+
 	function __construct() {
 		parent::__construct();
 		$this->load->model('madmin');
 		$this->madmin = new Madmin();
 	}
+
 	public function index() {
 		$this->company_data();
 	}
-	public function company_data($users=NULL) {
+
+	public function company_data($users = NULL) {
 		$data['pageTitle'] = 'General Company Data';
 		$post = $this->input->post();
 		if ($post) {
@@ -18,9 +22,9 @@ class Admin extends CI_Controller {
 			$this->madmin->insertCompanyData($post);
 		} elseif ($users) {
 			$data['users'] = $this->madmin->getUser();
-			$this->load->template("admin/company_data/general",$data);
+			$this->load->template("admin/company_data/general", $data);
 		} else {
-			$this->load->template("admin/company_data/general",$data);
+			$this->load->template("admin/company_data/general", $data);
 		}
 	}
 }
---
+++ application/controllers/packet.php
@@ -0,0 +1,32 @@
+<?php
+if (! defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class Packet extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this->load->model('mpacket');
+		$this->mpacket = new MPacket();
+	}
+
+	public function index() {
+		$this->resv();
+	}
+	
+	public function test(){
+		$post = $this->input->post();
+		print_r($post);
+	}
+
+	public function resv() {
+		$post = $this->input->post();
+		$data = $post['full'];
+		if($this->mpacket->insertPacket($data)){
+			echo 'success';
+		}
+	}
+
+	public function position() {
+	}
+}
