b32674b8c23e337c6c3c4be15c71dbb3adad885c
rizki182 <rizkifaishal82@gmail.com>
2014-02-06 10:40:35 +0700

    OPEN - task fixed #18: update map http://github.com/antareja/surabi/issues/issue/18

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -9,45 +9,7 @@
 	</div><!-- /.main-container -->
 
 	
-	<!-- basic scripts -->
-
-		<!--[if !IE]> -->
-
-		<script type="text/javascript">
-			window.jQuery || document.write("<script src='<?php echo site_url() ?>assets/js/jquery-2.1.0.min.js'>"+"<"+"/script>");
-		</script>
-
-		<!-- <![endif]-->
-
-		<!--[if IE]>
-<script type="text/javascript">
- window.jQuery || document.write("<script src='<?php echo site_url() ?>assets/js/jquery-2.1.0.min.js'>"+"<"+"/script>");
-</script>
-<![endif]-->
-
-		<script type="text/javascript">
-			if("ontouchend" in document) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
-		</script>
-
-		<!-- page specific plugin scripts -->
-
-		<!-- ace scripts -->
-
-
-		<!-- inline scripts related to this page -->
-    <!-- Core Scripts - Include with every page -->
-    <script src="<?php echo site_url() ?>assets/js/jquery-2.1.0.min.js"></script>
-    <!-- Map -->
-    <! -- Maps Google -->
-    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-	<script src="http://localhost:8000/socket.io/socket.io.js"></script>
-    <script src="<?php echo site_url() ?>assets/js/map.js"></script>	
-    <script src="<?php echo site_url() ?>assets/js/bootstrap.min.js"></script>
-
-	<script src="<?php echo site_url() ?>assets/js/typeahead-bs2.min.js"></script>
-	<!-- ace scripts -->
-	<script src="<?php echo site_url() ?>assets/js/ace-elements.min..js"></script>
-    <script src="<?php echo site_url() ?>assets/js/ace.min.js"></script>
+	
 
 
 </body>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,45 @@
 	href="<?php echo site_url()?>assets/ico/favicon.png">
 
 <title>GPS Tracker <?php echo (isset($pageTitle)) ? $pageTitle : ''; ?></title>
+<!-- basic scripts -->
 
+		<!--[if !IE]> -->
+
+		<script type="text/javascript">
+			window.jQuery || document.write("<script src='<?php echo site_url() ?>assets/js/jquery-2.1.0.min.js'>"+"<"+"/script>");
+		</script>
+
+		<!-- <![endif]-->
+
+		<!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='<?php echo site_url() ?>assets/js/jquery-2.1.0.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+
+		<script type="text/javascript">
+			if("ontouchend" in document) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+		</script>
+
+		<!-- page specific plugin scripts -->
+
+		<!-- ace scripts -->
+
+
+		<!-- inline scripts related to this page -->
+    <!-- Core Scripts - Include with every page -->
+    <script src="<?php echo site_url() ?>assets/js/jquery-2.1.0.min.js"></script>
+    <!-- Map -->
+    <! -- Maps Google -->
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+	<script src="http://localhost:8000/socket.io/socket.io.js"></script>
+    <script src="<?php echo site_url() ?>assets/js/map.js"></script>	
+    <script src="<?php echo site_url() ?>assets/js/bootstrap.min.js"></script>
+
+	<script src="<?php echo site_url() ?>assets/js/typeahead-bs2.min.js"></script>
+	<!-- ace scripts -->
+	<script src="<?php echo site_url() ?>assets/js/ace-elements.min..js"></script>
+    <script src="<?php echo site_url() ?>assets/js/ace.min.js"></script>
 <!-- Bootstrap core CSS -->
 <link href="<?php echo site_url()?>assets/css/bootstrap.min.css"
 	rel="stylesheet">
