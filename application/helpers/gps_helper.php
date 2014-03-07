c1ad955497f84e7050d61365410b957b495e8170
haidar mar'ie <coder5@ymail.com>
2014-03-07 18:56:35 +0700

    OPEN - task 68: Point in Polygon Function http://github.com/antareja/surabi/issues/issue/68

--- application/helpers/gps_helper.php
+++ application/helpers/gps_helper.php
@@ -48,11 +48,11 @@ function define_sess($username, $user_id, $full_name) {
 	// print_r($_SESSION);exit;
 }
 
-function user_id_sess(){
+function user_id_sess() {
 	return $_SESSION['gps_user_id'];
 }
 
-function username_sess(){
+function username_sess() {
 	return $_SESSION['gps_username'];
 }
 
@@ -64,6 +64,29 @@ function previous_url() {
 	} else {
 		return redirect('home');
 	}
+}
+
+# For check Point in Polygon
+function poly_contains($point, $polygon) {
+	if ($polygon[0] != $polygon[count($polygon) - 1])
+		$polygon[count($polygon)] = $polygon[0];
+	$j = 0;
+	$oddNodes = false;
+	$x = $point[1];
+	$y = $point[0];
+	$n = count($polygon);
+	for($i = 0; $i < $n; $i ++) {
+		$j ++;
+		if ($j == $n) {
+			$j = 0;
+		}
+		if ((($polygon[$i][0] < $y) && ($polygon[$j][0] >= $y)) || (($polygon[$j][0] < $y) && ($polygon[$i][0] >= $y))) {
+			if ($polygon[$i][1] + ($y - $polygon[$i][0]) / ($polygon[$j][0] - $polygon[$i][0]) * ($polygon[$j][1] - $polygon[$i][1]) < $x) {
+				$oddNodes = ! $oddNodes;
+			}
+		}
+	}
+	return $oddNodes;
 }
 
 function prev_url() {
