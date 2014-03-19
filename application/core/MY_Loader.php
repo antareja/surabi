d4c4f72fbc8bd14ca7e7955089c2480a1ef04d47
haidar mar'ie <coder5@ymail.com>
2014-03-19 09:13:41 +0700

    sidebar only on home

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -3,7 +3,11 @@ class MY_Loader extends CI_Loader {
 	public function template($template_name, $vars = array(), $return = FALSE)
 	{
 		$content  = $this->view('templates/header', $vars, $return);
-		$content .= $this->view('templates/sidebar', $vars, $return);
+// 		echo empty($this->uri->segment(1)) ? 'home': $this->uri->segment(1) ;exit;
+		if (empty($this->uri->segment(1)) || $this->uri->segment(1) == 'home') {
+// 			echo 'masuk';exit;
+			$content .= $this->view('templates/sidebar', $vars, $return);
+		}
 		$content .= $this->view('templates/breadcrump', $vars, $return);
 		$content .= $this->view($template_name, $vars, $return);
 		$content .= $this->view('templates/footer', $vars, $return);
