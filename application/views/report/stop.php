40ae01bd4b1cbf3fced5c18f5a3bd93105c638dc
haidar mar'ie <coder5@ymail.com>
2014-03-25 11:31:40 +0700

    OPEN - task fixed #82: Report Form Modification http://github.com/antareja/surabi/issues/issue/82

--- application/views/report/stop.php
+++ application/views/report/stop.php
@@ -5,6 +5,12 @@
 </style>
 <div id="header" align="center">
 	<h1><?php echo $pageTitle?></h1>
+	<form action="<?php echo site_url()?>report/<?php echo $this->uri->segment(2)?>/pdf" id="pdf_report" target="_blank" method="POST" >
+		<input type="hidden" name="begin" value="<?php echo isset($begin) ? $begin : ''?>">
+		<input type="hidden" name="end" value="<?php echo isset($end) ? $end : ''?>">
+		<input type="hidden" name="vehicle" value="<?php echo isset($vehicle) ? $vehicle : ''?>">
+		<input type="hidden" name="pdf" value="1">
+	<h4><?php echo isset($pdf) ? $pdf : '';?></h4>
 </div>
 <table width="100%">
 	<tr>
