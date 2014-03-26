d50bacb70d7be8f729595a72581196efcd1fd69c
haidar mar'ie <coder5@ymail.com>
2014-03-26 16:23:05 +0700

    OPEN - task 92: Add Total Duration Stop in Stop Report http://github.com/antareja/surabi/issues/issue/92

--- application/views/report/stop.php
+++ application/views/report/stop.php
@@ -1,39 +1,44 @@
 <script src="<?php echo base_url() ?>assets/js/jquery-2.1.0.min.js"></script>
 <script>
+function toSeconds(time) {
+	var parts = time.split(':');
+	return (+parts[0]) * 60 * 60 + (+parts[1]) * 60 + (+parts[2]);
+}
 
-		function toSeconds( time ) {
-		    var parts = time.split(':');
-		    return (+parts[0]) * 60 * 60 + (+parts[1]) * 60 + (+parts[2]); 
+function toHHMMSS(sec) {
+	var sec_num = parseInt(sec, 10); // don't forget the second parm
+	var hours = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var time = hours + ':' + minutes + ':' + seconds;
+	return time;
+}
+
+function calculateSum(id) {
+	var sum = 0;
+	$(".duration" + id).each(function() {
+
+		var value = $(this).text();
+		// add only if the value is number
+		if (!isNaN(value) && value.length != 0) {
+			sum += parseFloat(value);
 		}
-		
-		function toHHMMSS(sec) {
-		    var sec_num = parseInt(sec, 10); // don't forget the second parm
-		    var hours   = Math.floor(sec_num / 3600);
-		    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-		    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-		
-		    if (hours   < 10) {hours   = "0"+hours;}
-		    if (minutes < 10) {minutes = "0"+minutes;}
-		    if (seconds < 10) {seconds = "0"+seconds;}
-		    var time    = hours+':'+minutes+':'+seconds;
-		    return time;
-		}
-		
-		function calculateSum(id) {
-			var sum = 0;
-			$(".duration"+id).each(function() {
-		
-				var value = $(this).text();
-				// add only if the value is number
-				if (!isNaN(value) && value.length != 0) {
-					sum += parseFloat(value);
-				}
-			});
-		
-			$('.result'+id).text(sum);
-		};
+	});
+
+	$('.result' + id).text(sum);
+};
+
 		<?php 
-
 		$vehicle_id = '';
 		$i = 1;
 		foreach ($stop->result() as $row) {
@@ -49,7 +54,7 @@
 		    });
 		    
 		    $('.result<?php echo $row->vehicle_id?>').text( toHHMMSS(total) );
-		    
+		    $('.test').text('hai');
 		})
 		<?php }} ?>
 		</script>
@@ -57,6 +62,7 @@
 .ganjil {
 	background-color: #999999
 }
+
 </style>
 <div id="header" align="center">
 	<h1><?php echo $pageTitle?></h1>
@@ -83,10 +89,7 @@ $x = 1;
 $vehicle_name = '';
 $daily_total = array();
 $total = array();
-foreach ($stop->result() as $row) { ?>
-
-
-<?php 
+foreach ($stop->result() as $row) { 
 	$x ++;
 	if ($x % 2 == 0)
 		$class = "genap";
@@ -105,14 +108,15 @@ foreach ($stop->result() as $row) { ?>
 		<td colspan="5"><?php echo $vehicle_name?></td>
 	</tr>
 	<tr>
-		<td colspan="5" align="right" class="result<?php echo $row->vehicle_id?>">Total Stop Duration</td>
+		<td colspan="5">Total Stop Duration <span class="test"></span>: <span class="result<?php echo $row->vehicle_id?>"></span></td>
 	</tr>
 		<?php
 	} else {
 		array_push($daily_total[$row->name], $row->duration);
 	}
 	$total[$row->name] = sum_the_time($daily_total[$row->name]);
-	// echo 'total'.$row->name .$total[$row->name];
+// 	echo 'total'.$row->name .$total[$row->name];
+	print_r($total);
 	?>		
 		<tr class="<?php echo $class?>">
 		<td><?php echo $row->date ?></td>
