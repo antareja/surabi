cb0adcb9b74b668838a0fc4f79154322f20eee51
haidar mar'ie <coder5@ymail.com>
2014-04-22 23:43:53 +0800

    replay bugs fix

--- application/controllers/replay.php
+++ application/controllers/replay.php
@@ -18,39 +18,28 @@ class Replay extends CI_Controller {
 	public function index() {
 		$this->replay2();
 	}
-	
+
 	public function replay() {
 		$data['pageTitle'] = "Replay Module";
 		$mobile_address = '00000000000000000000000521';
 		$data['replay'] = $this->mpacket->getReplay($mobile_address);
-		$this->load->template('replay',$data);
+		$this->load->template('replay', $data);
 	}
-	
+
 	public function replay2() {
 		$show = 'only';
 		$this->load->model('admin/mfleet_config');
 		$this->mfleet_config = new MFleet_config();
 		$data['pageTitle'] = "Replay Module";
 		$post = $this->input->post();
-		if($post)
-		{
-			$mobile_address=$this->input->post("gps_mobile_address");
-			$tanggal=$this->input->post("tanggal");
-			$tanggal=date("Y-m-d", strtotime($tanggal));
-			$data['data_replay'] = $this->mpacket->getReplayData($mobile_address,$tanggal);
+		if ($post) {
+			$mobile_address = $this->input->post("gps_mobile_address");
+			$tanggal = $this->input->post("tanggal");
+			$tanggal = date("Y-m-d", strtotime($tanggal));
+			$data['data_replay'] = $this->mpacket->getReplayData($mobile_address, $tanggal);
+		} else {
+			$data['allvehicle'] = $this->mfleet_config->getAllVehicle($_SESSION['gps_user_id'], $show);
 		}
-		else
-		{
-			if($_SESSION['gps_level'] == 'operator') {
-				$data['allvehicle'] = $this->mfleet_config->getAllVehicleUser($_SESSION['gps_user_id'], $show);
-			} elseif ($_SESSION['gps_level'] == 'admin_vendor') {
-				$data['allvehicle'] = $this->mfleet_config->getAllVehicleAdminVendor($_SESSION['gps_company_id'], $show);
-			} elseif ($_SESSION['gps_level'] == 'admin') {
-				$data['allvehicle'] = $this->mfleet_config->getAllVehicleAdmin($show);
-				// 			$data['vehicles'] = $this->mgps->getDataVehicle();
-			}
-		}
-		$this->load->template('replay2',$data);
+		$this->load->template('replay2', $data);
 	}
-	
 }
